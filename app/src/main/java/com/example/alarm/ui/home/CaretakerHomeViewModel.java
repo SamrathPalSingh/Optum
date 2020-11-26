92ac657ef74632b08d2cf232e2060cc6ba805ffd
InvincibleSarthak <eccentric.sarthak@gmail.com>
2020-11-26 23:55:05 +0530

    caretaker and doctor home ui, done

---
+++ app/src/main/java/com/example/alarm/ui/home/CaretakerHomeViewModel.java
@@ -0,0 +1,7 @@
+package com.example.alarm.ui.home;
+
+import androidx.lifecycle.ViewModel;
+
+public class CaretakerHomeViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/example/alarm/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/alarm/ui/home/HomeFragment.java
@@ -4,44 +4,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.alarm.R;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private ToggleButton toggle;
     private TextView t;
-
+    private Switch s;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        t = root.findViewById(R.id.textView2);
-        toggle = (ToggleButton) root.findViewById(R.id.togglebutton);
-        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                        setZoomOn();
-                        t.setText("https://meet.google.com/pfs-kavj-man");
-                } else {
-                    setZoomOff();
-                    t.setText("");
-                }
-            }
-        });
+        View root = inflater.inflate(R.layout.fragment_doctor_home, container, false);
+        s = root.findViewById(R.id.status_switch);
+        s.setChecked(false);
+        if(s.isChecked()){
+            //Go Online
+        }
+        else{
+            //Remain offline
+        }
+//        t = root.findViewById(R.id.textView2);
+//        toggle = (ToggleButton) root.findViewById(R.id.togglebutton);
+//        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                        setZoomOn();
+//                        t.setText("https://meet.google.com/pfs-kavj-man");
+//                } else {
+//                    setZoomOff();
+//                    t.setText("");
+//                }
+//            }
+//        });
 //        final TextView textView = root.findViewById(R.id.text_home);
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
 //            @Override
@@ -51,18 +54,18 @@ public class HomeFragment extends Fragment {
 //        });
         return root;
     }
-    private void setZoomOff() {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Doctor");
-
-        // Write from the database
-        myRef.child("1").child("Status").setValue("1");
-    }
-    private void setZoomOn() {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Doctor");
-
-        // Write from the database
-        myRef.child("1").child("Status").setValue("0");
-    }
+//    private void setZoomOff() {
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("Doctor");
+//
+//        // Write from the database
+//        myRef.child("1").child("Status").setValue("1");
+//    }
+//    private void setZoomOn() {
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("Doctor");
+//
+//        // Write from the database
+//        myRef.child("1").child("Status").setValue("0");
+//    }
 }
